74503af018c3b72009f6d967293c5cc1c0e6c508
WAYNEisBATMAN <64163400+WAYNEisBATMAN@users.noreply.github.com>
2020-09-01 02:08:33 +0530

    vsc

---
+++ 1. Datatypes/Comprehensions.py
@@ -0,0 +1,66 @@
+"""
+/ COMPREHENSIONS
+Comprehensions in Python provide us with a short and concise way to construct new sequences (such as lists, set, dictionary etc.)
+using sequences which have been already defined. Python supports the following 4 types of comprehensions:
+"""
+#-------------------------------------------------------------------------------------------------------------------------
+
+"""
+. 1) List Comprehensions:
+
+
+
+"""
+
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+"""
+. 2) Dictionary Comprehensions:
+
+
+
+"""
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+"""
+. 3) Set Comprehensions:
+
+
+
+
+"""
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+"""
+. 4) Generator Comprehensions:
+
+
+
+
+"""
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+# > https://www.geeksforgeeks.org/comprehensions-in-python/
